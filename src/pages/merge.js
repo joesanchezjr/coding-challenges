6c92f6ad176cf47160daf0dd9fd8ab71e85c3a06
Joe Sanchez Jr <joe@joesanchezjr.com>
2021-07-23 13:37:04 -0500

    merge page styles

--- src/pages/merge.js
+++ src/pages/merge.js
@@ -22,18 +22,21 @@ export default function OverlappingItems() {
         <section className="mb-4">
           <p>
             View code used for merged arrays here:{" "}
-            <a href="https://github.com/joesanchezjr/coding-challenges/blob/9bd2503c855cca1d01daedb0b4363692109fc481/utils/merge.js" className="underline">
+            <a
+              href="https://github.com/joesanchezjr/coding-challenges/blob/9bd2503c855cca1d01daedb0b4363692109fc481/utils/merge.js"
+              className="underline"
+            >
               GitHub Repository
             </a>
           </p>
         </section>
-        <section className="flex space-x-4">
+        <section className="flex space-x-4 max-w-full overflow-x-scroll">
           <div>
-            <h2>Original Array</h2>
+            <h2 className="text-xl font-bold">Original Array</h2>
             <pre>{JSON.stringify(items, null, 2)}</pre>
           </div>
           <div>
-            <h2>Sorted Array</h2>
+            <h2 className="text-xl font-bold">Sorted Array</h2>
             <pre>
               {JSON.stringify(
                 [...items].sort((a, b) => a.startPx - b.startPx),
@@ -43,11 +46,11 @@ export default function OverlappingItems() {
             </pre>
           </div>
           <div>
-            <h2>Merged Array</h2>
+            <h2 className="text-xl font-bold">Merged Array</h2>
             <pre>{JSON.stringify(merge(items), null, 2)}</pre>
           </div>
           <div>
-            <h2>Merged Array Alt</h2>
+            <h2 className="text-xl font-bold">Merged Array Alt</h2>
             <pre>{JSON.stringify(mergeAlt(items), null, 2)}</pre>
           </div>
         </section>
